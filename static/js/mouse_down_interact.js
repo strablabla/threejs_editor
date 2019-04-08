593572754e8e63d7275fb0b7e3fead0002b21973
aspilio <lio.chiron@google.com>
2019-04-08 23:05:15 +0200

    continuing to reorganize the code

---
+++ static/js/mouse_down_interact.js
@@ -0,0 +1,35 @@
+
+/*
+
+Mouse_down
+
+*/
+
+function mouse_down_case_intersections(intersects,raycaster){
+
+      /*
+      One intersection or more detected
+      */
+
+      controls.enabled = false;
+      SELECTED = intersects[ 0 ].object;
+      intersects = raycaster.intersectObject( plane );
+      container.style.cursor = 'move';
+
+      return intersects
+}
+
+function MouseDown_actions(){
+
+      /*
+      Mouse down actions
+      */
+
+      if ( INTERSECTED ) INTERSECTED.material.color.setHex( color_intersected_green );   // changing color in green when selected
+      if (select_picking){ picking_action() }                             // adding the object to the list of the picked elements..
+      if (select_move_group){ keep_relative_positions() }                 // save the relative positions inside the group
+      if (select_make_track){
+          if (list_marks_track.length > 1){ make_oriented_track() }  // Draw the track
+      }
+
+}
