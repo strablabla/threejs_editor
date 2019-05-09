17a4a5d75d55ffb94e635a011428818cca09f57e
aspilio <lio.chiron@google.com>
2019-05-09 13:16:18 +0200

    adding repusivity for elastics

--- static/js/create_object_interact.js
+++ static/js/create_object_interact.js
@@ -15,10 +15,8 @@ function gaussianRand() {
       */
 
       var rand = 0;
-      for (var i = 0; i < 6; i += 1) {
-            rand += Math.random();
-      }
-
+      for (var i = 0; i < 6; i += 1) { rand += Math.random() }
+      
       return rand / 6;
 }
 
